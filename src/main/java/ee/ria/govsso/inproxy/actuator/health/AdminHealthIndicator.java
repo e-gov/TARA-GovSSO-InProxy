f4d05781346ce3d67995f569df9117dda6e00ae7
Boriss Melikjan <boriss.melikjan@nortal.com>
2026-04-27 12:40:43 +0300

    AUT-2752 Migrate Actuator health imports for Boot 4

--- src/main/java/ee/ria/govsso/inproxy/actuator/health/AdminHealthIndicator.java
+++ src/main/java/ee/ria/govsso/inproxy/actuator/health/AdminHealthIndicator.java
@@ -3,8 +3,8 @@ package ee.ria.govsso.inproxy.actuator.health;
 import ee.ria.govsso.inproxy.service.TokenRequestAllowedIpAddressesService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.health.contributor.Health;
+import org.springframework.boot.health.contributor.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 @Slf4j
--- src/test/java/ee/ria/govsso/inproxy/actuator/health/certificates/CertificatesHealthIndicatorTest.java
+++ src/test/java/ee/ria/govsso/inproxy/actuator/health/certificates/CertificatesHealthIndicatorTest.java
@@ -1,7 +1,7 @@
 package ee.ria.govsso.inproxy.actuator.health.certificates;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.health.contributor.Status;
 
 import java.util.ArrayList;
 import java.util.List;
