a334deb2f6d9c3fa6471b951674bbfb0ca7dc954
Sander Kondratjev <sander.kondratjev@nortal.com>
2026-06-26 14:42:31 +0300

    AUT-2752 Remove WRITE_DATES_AS_TIMESTAMPS

--- src/main/java/ee/ria/govsso/inproxy/logging/LogbackMapperBuilderDecorator.java
+++ src/main/java/ee/ria/govsso/inproxy/logging/LogbackMapperBuilderDecorator.java
@@ -2,9 +2,7 @@ package ee.ria.govsso.inproxy.logging;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import net.logstash.logback.decorate.MapperBuilderDecorator;
-import tools.jackson.databind.cfg.DateTimeFeature;
 import tools.jackson.databind.PropertyNamingStrategies;
-import tools.jackson.databind.SerializationFeature;
 import tools.jackson.databind.json.JsonMapper;
 import tools.jackson.databind.util.StdDateFormat;
 
@@ -13,7 +11,6 @@ public class LogbackMapperBuilderDecorator implements MapperBuilderDecorator<Jso
     @Override
     public JsonMapper.Builder decorate(JsonMapper.Builder builder) {
         return builder
-                .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .defaultDateFormat(new StdDateFormat().withColonInTimeZone(false))
                 .changeDefaultPropertyInclusion(inclusion -> inclusion.withValueInclusion(JsonInclude.Include.NON_NULL))
                 .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
